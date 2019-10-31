6b2c06ffe4d988c39ef1affdd5338feaf020acbe
gonuit <kamilklyta341@gmail.com>
2019-10-31 22:51:02 +0100

    Add documentation for CustomRefreshIndicatorState enum

--- lib/src/definitions.dart
+++ lib/src/definitions.dart
@@ -8,19 +8,42 @@ typedef CustomIndicatorBuilder = Widget Function(
 
 /// Describes state of CustomRefreshIndicator
 enum CustomRefreshIndicatorState {
-  /// Indicator should be idle
+  /// #### [CustomRefreshIndicator] is idle (There is no action)
+  ///
+  /// (`CustomRefreshIndicatorData.value == 0`
   idle,
 
-  /// List is dragged
+  /// #### Whether user is draging [CustomRefreshIndicator]
+  /// ending the scroll **WILL NOT** result in `onRefresh` call
+  ///
+  /// (`CustomRefreshIndicatorData.value < 1`)
   draging,
 
-  /// Indicator is dragged enough to be trigger `onRefresh` action
+  /// #### [CustomRefreshIndicator] is armed
+  /// ending the scroll **WILL** result in:
+  /// - `CustomRefreshIndicator.onRefresh` call
+  /// - change of status to `loading`
+  /// - decreasing `CustomRefreshIndicatorData.value` to `1` in
+  /// duration specified by `CustomRefreshIndicator.armedToLoadingDuration`)
+  ///
+  /// (`CustomRefreshIndicatorData.value >= 1`)
   armed,
 
-  /// hiding indicator when `onRefresh` action is done or indicator was canceled
+  /// #### [CustomRefreshIndicator] is hiding indicator
+  /// when `onRefresh` future is resolved or indicator was canceled
+  /// (scroll ended when [CustomRefreshIndicatorState] was equal to `dragging`
+  /// so `value` was less than `1` or the user started scrolling through the list)
+  ///
+  /// (`CustomRefreshIndicatorData.value` decreases to `0`
+  /// in duration specified by `CustomRefreshIndicator.dragingToIdleDuration`)
   hiding,
 
-  /// `onRefresh` action is pending
+  /// #### [CustomRefreshIndicator] is awaiting on `onRefresh` call result
+  /// When `onRefresh` will resolve [CustomRefreshIndicator] will change state
+  /// from `loading` to `hiding` and decrease `CustomRefreshIndicatorData.value`
+  /// from `1` to `0` in duration specified by `CustomRefreshIndicator.loadingToIdleDuration`
+  ///
+  /// (`CustomRefreshIndicatorData.value == 1`)
   loading,
 }
 
@@ -34,17 +57,43 @@ class CustomRefreshIndicatorData {
     @required this.indicatorState,
   });
 
+  /// Direction in which user is scrolling
   final ScrollDirection scrollingDirection;
+
+  /// Scrolling is happening in the positive scroll offset direction.
   bool get isScrollingForward => scrollingDirection == ScrollDirection.forward;
+
+  /// Scrolling is happening in the negative scroll offset direction.
   bool get isScrollingReverse => scrollingDirection == ScrollDirection.reverse;
+
+  /// No scrolling is underway.
   bool get isScrollIdle => scrollingDirection == ScrollDirection.idle;
 
+  /// The direction in which list scrolls
+  ///
+  /// For example:
+  /// ```
+  /// ListView.builder(
+  ///   scrollDirection: Axis.horizontal,
+  ///   reverse: true,
+  ///   // ***
+  /// ```
+  /// will have the direction of `AxisDirection.left`
   final AxisDirection direction;
+
+  /// Whether list scrolls horrizontally
+  ///
+  /// (direction equals `AxisDirection.left` or `AxisDirection.right`)
   bool get isHorizontalDirection =>
       direction == AxisDirection.left || direction == AxisDirection.right;
+
+  /// Whether list scrolls vertically
+  ///
+  /// (direction equals `AxisDirection.up` or `AxisDirection.down`)
   bool get isVerticalDirection =>
       direction == AxisDirection.up || direction == AxisDirection.down;
 
+  /// Describes the state in which [CustomRefreshIndicator] is
   final CustomRefreshIndicatorState indicatorState;
   bool get isArmed => indicatorState == CustomRefreshIndicatorState.armed;
   bool get isDraging => indicatorState == CustomRefreshIndicatorState.draging;
